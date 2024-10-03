663749af31116d2922839638ebe01e5c505fec2e
unknown <agapi.andreea1826@yahoo.com>
2024-10-03 14:49:47 +0300

    Am adaugat tema de la sprint 7 - Intro JS

---
+++ sprint 7/tema 1.js
@@ -0,0 +1,11 @@
+function divNumber(number){
+
+    if(number % 10 === 0){
+        console.log(true);
+    } else{
+        console.log(false);
+    }
+}
+
+// divNumber(100)
+divNumber(23)
---
+++ sprint 7/tema 2.js
@@ -0,0 +1,7 @@
+function tellFortune(kidsNumber, partenerName, location, workSpace){
+
+    return `Vei fi un ${workSpace} în ${location}, căsătorit cu ${partenerName} și vei avea ${kidsNumber} copii.`
+
+}
+
+console.log(tellFortune(3, "Daniel", "Bucuresti", "programator"))
