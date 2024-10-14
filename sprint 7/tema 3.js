fc1d61e0858bc0269033a0cad7b35ed7a466c1c6
unknown <agapi.andreea1826@yahoo.com>
2024-10-14 13:20:57 +0300

    Am adaugat tema de  la sprint 7

---
+++ sprint 7/tema 3.js
@@ -0,0 +1,46 @@
+function dayOfTheWeek(weekDay) {
+  
+  let dayName = parseInt(weekDay);
+
+  switch(dayName) {
+    case 1:
+      return "Monday";
+
+    case 2:
+      return "Tuesday";
+
+    case 3:
+      return "Wednesday";
+
+    case 4:
+      return "Thursday";
+
+    case 5:
+      return "Friday";
+
+    case 6:
+      return "Saturday";
+
+    case 7:
+    case 0:
+      return "Sunday";
+
+    default:
+      return "You have to write a number between 0 and 7.";
+  }
+
+}
+  
+
+function dayReturn(dayNumber) {
+
+  if (dayNumber === 0 || dayNumber <= 7) {
+    return "Today is " + dayOfTheWeek(dayNumber) + ".";
+  } else {
+    return dayOfTheWeek(dayNumber);
+  }
+
+}
+
+  // console.log(dayReturn(2));
+  console.log(dayReturn(11));
