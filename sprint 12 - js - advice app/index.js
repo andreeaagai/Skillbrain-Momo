7a51322ed15d6564c2919a7061015d4635743505
unknown <agapi.andreea1826@yahoo.com>
2024-11-16 12:46:37 +0200

    Am refacut tema pentru Advice

---
+++ sprint 12 - js - advice app/index.js
@@ -0,0 +1,31 @@
+const adviceIdElement = document.getElementById("adviceId");
+const adviceTextElement = document.getElementById("adviceText");
+const diceButton = document.getElementById("button");
+
+
+const functionWhenDOMLoaded = () => {
+    const el =  document.getElementById("button");
+    el.onclick = () => fetchAdvice();
+}
+
+async function fetchAdvice() {
+    const apiResponse = "https://api.adviceslip.com/advice";
+    let adviceText = "";
+    let adviceId = "";
+
+    try {
+        const response = await fetch(apiResponse);
+        const adviceData = await response.json();
+
+        adviceText = `${adviceData.slip.advice}`;
+        adviceId = adviceData.slip.id;
+    } 
+    catch (error) {
+        console.error(error);
+    }
+
+    document.getElementById("adviceId").textContent = `Advice #${adviceId}`;
+    document.getElementById("adviceText").textContent = `"${adviceText}"`;
+}
+
+document.addEventListener("DOMContentLoaded", functionWhenDOMLoaded);
